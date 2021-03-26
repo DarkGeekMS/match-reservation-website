34aefa2d85a91bcfe0dc1830411ec6c9d8a2e818
RemondaTalaat <remondatalaat21@gmail.com>
2021-03-26 02:24:59 +0200

    approve and delete user end points

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -32,9 +32,6 @@ class AdminController extends Controller
      * @response 200{
      * "user_id": "approved"
      * }
-     * @response  400{
-     * "error": "not approved"
-     * }
      * @response  404{
      * "error": "user not found"
      * }
@@ -50,7 +47,14 @@ class AdminController extends Controller
 
     public function approveUser(Request $request) 
     {
-        return;
+        try {
+            $user = User::withTrashed()->find( $request['user_id']);
+            $user->verified = true;
+            $user->save();
+            return response()->json([$request['user_id'] => 'approved' ], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'user not found' ], 404);
+        }
     }
 
     /**
@@ -62,19 +66,20 @@ class AdminController extends Controller
     * Failure Cases:
     * 1) user can't be deleted.
     * 2) user not found.
+    * 3) try to delete another admin.
     * 
     * 
     * @bodyParam user_id int required Used to get the user to be approved.
     * @bodyParam token   JWT required Used to verify the logged in user.
     * 
     * @response 200{
-    * "": ""
+    * "user_id": "deleted"
     * }
     * @response  400{
-    * "error": "error_message"
+    * "error": "admin users cannot be deleted"
     * }
     * @response  404{
-    * "error": "not_found_error_message"
+    * "error": "user not found"
     * }
     */
     /**
@@ -89,7 +94,16 @@ class AdminController extends Controller
 
     public function deleteUser(Request $request) 
     {
-        return;
+        try {
+            $user = User::withTrashed()->where('id', $request['user_id'])->first();
+            if($user['role'] == 3) {
+                return response()->json(['error' => 'admin users cannot be deleted' ], 400);
+            }
+            $user->forceDelete();
+            return response()->json([$request['user_id'] => 'deleted' ], 200);
+        } catch (\Throwable $th) {
+            return response()->json(['error' => 'user not found' ], 404);
+        }
     }
 
     /**
@@ -138,7 +152,7 @@ class AdminController extends Controller
             return response()->json(['error' => "you have to be admin to view this data" ], 400);
         }
         // return the verified users
-        $users = User::where('verified', true)->select(['username','role'])->get();
+        $users = User::where('verified', true)->select(['username','role', 'id'])->get();
         return response()->json(['users' => $users ], 200);
     }
 
@@ -194,7 +208,7 @@ class AdminController extends Controller
             return response()->json(['error' => "you have to be admin to view this data" ], 400);
         }
         // return the unverified users
-        $users = User::where('verified', false)->select(['username','role'])->get();
+        $users = User::where('verified', false)->select(['username','role', 'id'])->get();
         return response()->json(['users' => $users ], 200);
     }
 }
