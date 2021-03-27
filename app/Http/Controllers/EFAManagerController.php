c6bd3e8dfd957eb92a4eebc2c5c4d1006cdc2459
DarkGeekMS <mohamedshawky911@gmail.com>
2021-03-27 10:00:25 +0200

    complete createMatch endpoint

--- app/Http/Controllers/EFAManagerController.php
+++ app/Http/Controllers/EFAManagerController.php
@@ -16,31 +16,40 @@ class EFAManagerController extends Controller
 
     /**
     * createMatch
-    * shot description 
+    * Creates a new match event with given data.
+    *
     * Success Cases :
-    * 1)
+    * 1) Match created successfully and added to database.
     * 
     * Failure Cases:
-    * 1)
+    * 1) Match teams not passed correctly.
+    * 2) Match data not passed correctly.
+    * 3) Match venue does not exist.
     * 
-    * 
-    * @bodyParam 
-    * @bodyParam 
+    * @bodyParam home_team       string  required The match home team
+    * @bodyParam away_team       string  required The match away team
+    * @bodyParam match_venu      integer required The match stadium
+    * @bodyParam main_referee    string  required The match main refree
+    * @bodyParam first_linesman  string  required The match first linesman
+    * @bodyParam second_linesman string  required The match second linesman
+    * @bodyParam date            Date    required The match date
+    * @bodyParam time            Time    required The match time
     * 
     * @response 200{
-    * "": ""
+    *   "New match created successfully"
     * }
     * @response  400{
-    * "error": "error_message"
+    * "error": "Enter the match teams correctly"
     * }
-    * @response  404{
-    * "error": "not_found_error_message"
+    * @response  400{
+    * "error": "Enter complete and correct match information"
+    * }
+    * @response  400{
+    * "error": "Error creating match, check match venue"
     * }
     */
     /**
-    * create new match event.
-    *
-    * function description here.
+    * Creates a new match event with given data.
     *
     * @param Request $request  
     *
@@ -49,7 +58,49 @@ class EFAManagerController extends Controller
 
     public function createMatch(Request $request) 
     {
-        return;
+        // validate team names
+        $home_team_validator = Validator::make($request->all(), ['home_team' => 'required|string|max:50']);
+        $away_team_validator = Validator::make($request->all(), ['away_team' => 'required|string|max:50']);
+
+        if ($home_team_validator->fails() || $away_team_validator->fails()) {
+            return response()->json(['error' => 'Enter the match teams correctly'], 400);
+        }
+
+        // validate rest of data
+        $rest_validator = Validator::make(
+            $request->all(), [
+            'match_venu'      => 'required|integer',
+            'main_referee'    => 'required|string|alpha_dash|max:50',
+            'first_linesman'  => 'required|string|alpha|max:50',
+            'second_linesman' => 'required|string|alpha|max:50',
+            'date'            => 'required|Date',
+            'time'            => 'required|date_format:H:i'
+            ]
+        );
+
+        if ($rest_validator->fails()) {
+            return response()->json(['error' => 'Enter complete and correct match information'], 400);
+        }
+
+        // get next match id
+        $last_match = Match::withTrashed()->get()->max('id');
+        $id = $last_match +1;
+
+        // create request data and add match id
+        $requestData = $request->all();
+        $requestData['id'] = $id;
+
+        // try creating match
+        try {
+            // sucessful insertion
+            $match = new Match($requestData);
+            $match->id = $id;
+            $match->save();
+            return response()->json('New match created successfully', 200);
+        } catch (\Throwable $th) {
+            // insertion failure
+            return response()->json(['error' => 'Error creating match, check match venue'], 400);
+        }
     }
 
     /**
